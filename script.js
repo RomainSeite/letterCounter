36a394dc1d2bbfe1584810e15664f91de43229e4
Romainseite <seite.romain@gmail.com>
2021-03-23 16:28:42 +0100

    works for consonants and vowels"

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
-(function() {
-    let countBtn = document.getElementById('countBtn')
+const countBtn = document.getElementById('countBtn') //bouton
+countBtn.addEventListener('click', onCountBtnClick) 
 
-    /* Compteur de nombre de consonnes 
-    * dans une chaine de caractères
-    */
-    function consonantCounter(str) {
-        let vowels = ['a', 'e', 'i', 'y', 'o', 'u']
-        let textContent = str.split('')
-        let consonantCount = 0;
-        //isNaN() : méthode qui retourne true si la chaine de caractère n'est pas un nombre
-        //A voir dans : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-        for(let i=0; i<textContent.length; i++) {
-            if(!vowels.includes(textContent[i].toLowerCase()) && isNaN(textContent[i]))
-                consonantCount++
-        }
+/* Compte le nombre de consonnes et de voyelles
+*  dans le paramètre string de type String
+*/
+function letterCounter(string) {
+        const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+        let vowelCount = 0,
+            consonantCount = 0;
+        //fait un tableau avec uniquement les lettres en minuscule pour comparer avec vowels
+        let letters = string.toLowerCase().split('').filter(letter => !(letter.toLowerCase() === letter.toUpperCase()));
+        letters.forEach(letter => {
+            vowels.includes(letter)? vowelCount++ : consonantCount++;
+        })
+        return consonantCount + " " + vowelCount;
+}
 
-        return consonantCount
-    }
-
-    function showText(textContent) {
-        let consonantCount = consonantCounter(textContent)
-        let divElement = document.getElementById('countedText')
-        let spanText = document.querySelector('span')
-        if(spanText) spanText.remove() 
-        let textLabel = document.createElement('span')
-        textLabel.textContent = `Il y a : ${consonantCount} consonne${consonantCount > 1 ? 's' : ''} dans la chaine : ${textContent}`
-        divElement.appendChild(textLabel)
-
-    }
+/* Affiche le nombre de consonnes et de voyelles
+*  dans un span
+*/
+function showText(textContent) {
+    let letterCount = letterCounter(textContent).split(' ');
+    let consonantCount = letterCount[0];
+    let vowelCount = letterCount[1];
+    let divElement = document.getElementById('countedText')
+    let spanText = document.querySelector('span')
+    if(spanText) 
+        spanText.remove() 
+    let textLabel = document.createElement('span')
+    textLabel.textContent = `There are ${consonantCount} consonant${consonantCount > 1 ? 's' : ''}
+                             and ${vowelCount} vowel${vowelCount > 1 ? 's' : ''} in the string : ${textContent}`
+    divElement.appendChild(textLabel)
+}
     
--   /*
-    * Fonction de Callback pour l'event listener
-    */
-    function onCountBtnClick() {
-        let textValue = document.getElementById('textContent').value
-        if(textValue.length > 0)
-            showText(textValue)
-    }
-
-    countBtn.addEventListener('click', onCountBtnClick)
-    
-})()
+/* Fonction executée lors du clique
+*  appelle showText() en lui envoyant
+*  comme argument les nombres de consonnes
+*  et de voyelles calculés par letterCounter()
+*/
+function onCountBtnClick() {
+    let textValue = document.getElementById('textContent').value;
+    if(textValue.length > 0)
+        showText(textValue)
+}
